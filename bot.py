03551608028ee14a5d562b7596acd046355004a5
GOSPODIN8 <axunovk10@gmail.com>
2026-08-30 16:14:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -224,7 +224,7 @@ async def cmd_stats(message: Message):
 async def main():
     db.init_db()
     logger.info("Бот запущен, ожидаю сообщения...")
-    await dp.start_polling(bot)
+    await bot.delete\_webhook drop\_pending\_updates=True\ await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
